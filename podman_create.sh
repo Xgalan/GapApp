a0e50839a436497254bb7524e2a789015413160b
Xgalan <erik.mascheri@gmail.com>
2023-05-02 21:28:50 +0200

    update podman scripts, added ER diagram

--- podman_create.sh
+++ podman_create.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+source .env.podman
 podman create --replace --name gapapp-v0.2 \
 --env-file=./gap/.env \
 -u root --userns=keep-id --group-add=keep-groups \
 -v=gapapp_vol:/usr/src/app:Z \
+-v=$DATABASE_DIR:/srv/databases:Z \
 --network=slirp4netns:allow_host_loopback=true \
--p=8080:8080 \
+-p=$CT_PORT:8000 \
 gapapp-v0.2:latest \
 gunicorn --worker-tmp-dir /dev/shm --config ./gap/gunicorn.conf.py gap.wsgi:application
